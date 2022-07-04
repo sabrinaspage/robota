ad64e3d520a9b4c30cc6d0bb35fd28302ec1faa8
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-07-04 16:02:05 -0400

    add navigation bar and Hero section

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,52 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Navbar, { NavLinkType } from "../components/Navbar";
+import { ButtonTypes } from "../components/RobotaButton";
+
+const homepageLinks: NavLinkType[] = [
+  {
+    title: "About Us",
+    urlPath: "#",
+  },
+  {
+    title: "Our Program",
+    urlPath: "#",
+  },
+  {
+    title: "Community",
+    urlPath: "#",
+  },
+  {
+    title: "Hiring Partners",
+    urlPath: "#",
+  },
+];
+
+const homepageButtons: NavLinkType[] = [
+  {
+    title: "Apply",
+    urlPath: "#",
+    type: ButtonTypes.OUTLINE_LARGE,
+  },
+  {
+    title: "Hire",
+    urlPath: "#",
+    type: ButtonTypes.CONTAINED_LARGE,
+  },
+  {
+    title: "ENG",
+    urlPath: "#",
+    type: ButtonTypes.CONTAINED_LARGE,
+  },
+];
 
 function Homepage() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <Navbar links={homepageLinks} buttons={homepageButtons} />
+      <Hero />
+    </div>
+  );
 }
 
 export default Homepage;
