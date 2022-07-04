87277569b8b0f08a844018364411ee67fba58c81
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-07-04 16:01:05 -0400

    navigation bar and buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,82 @@
-interface NavLink {
+import RobotaLogo from "../images/Robota.svg";
+import RobotaButton, { ButtonTypes } from "./RobotaButton";
+
+export interface NavLinkType {
   title: string;
   urlPath: string;
+  type?: ButtonTypes;
 }
 
-interface NavbarProps {
-  logo: string;
-  links: NavLink[];
-  buttons: NavLink[];
+export interface NavbarProps {
+  links: NavLinkType[];
+  buttons: NavLinkType[];
 }
 
-const Navbar = ({ logo, links, buttons }: NavbarProps) => (
-  <div>about us our program community hiring partners</div>
+const NavLink = ({ title, urlPath }: NavLinkType) => {
+  return (
+    <li className="nav-item" key={title}>
+      <a className="nav-link" href={urlPath}>
+        {title}
+      </a>
+    </li>
+  );
+};
+
+const NavLinks = ({ links, buttons }: NavbarProps) => {
+  return (
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        {links.map(({ title, urlPath }) => (
+          <NavLink
+            key={title}
+            {...{
+              title,
+              urlPath,
+            }}
+          />
+        ))}
+      </ul>
+      <span className="navbar-text">
+        <form className="container-fluid justify-content-start">
+          {buttons.map((button) => {
+            const { title, urlPath, type } = button;
+            if (type)
+              return (
+                <RobotaButton
+                  key={title}
+                  marginWidth="me-2"
+                  {...{ title, urlPath, type }}
+                />
+              );
+          })}
+        </form>
+      </span>
+    </div>
+  );
+};
+
+const Navbar = ({ links, buttons }: NavbarProps) => (
+  <nav className="navbar navbar-expand-lg bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">
+        <div className="container">
+          <img src={RobotaLogo} alt="" width="100" height="100" />
+        </div>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <NavLinks links={links} buttons={buttons} />
+    </div>
+  </nav>
 );
 
 export default Navbar;
--- src/components/RobotaButton.tsx
+++ src/components/RobotaButton.tsx
@@ -1,25 +1,54 @@
-enum ButtonTypes {
-  LARGE,
-  SMALL,
-  BLANK,
+export enum ButtonTypes {
+  OUTLINE_SMALL,
+  CONTAINED_SMALL,
+  OUTLINE_LARGE,
+  CONTAINED_LARGE,
 }
 
 interface SmallButtonProps {
   title: string;
   type: ButtonTypes;
+  urlPath: string;
+  marginWidth?: string;
 }
 
-const RobotaButton = ({ title, type }: SmallButtonProps) => {
-  if (type == ButtonTypes.LARGE) {
-    return <button> {title} </button>;
+const RobotaButton = ({
+  title,
+  type,
+  urlPath,
+  marginWidth,
+}: SmallButtonProps) => {
+  if (type == ButtonTypes.OUTLINE_LARGE) {
+    return (
+      <button
+        role="button"
+        type="button"
+        className={`btn btn-outline-primary btn-border-radius-sm btn-box-shadow ${marginWidth} fw-bold`}
+        onClick={() => urlPath}
+      >
+        {title}
+      </button>
+    );
   }
-  if (type == ButtonTypes.SMALL) {
-    return <button> {title} </button>;
+  if (type == ButtonTypes.CONTAINED_LARGE) {
+    return (
+      <a
+        role="button"
+        type="button"
+        className={`btn btn-primary btn-border-radius-sm btn-box-shadow ${marginWidth} text-white fw-bold`}
+        href={urlPath}
+      >
+        {title}
+      </a>
+    );
   }
-  if (type == ButtonTypes.BLANK) {
-    return <button> {title} </button>;
+  if (type == ButtonTypes.OUTLINE_SMALL) {
+    return <a href={urlPath}> {title} </a>;
   }
-  return;
+  if (type == ButtonTypes.CONTAINED_SMALL) {
+    return <a href={urlPath}> {title} </a>;
+  }
+  return <a></a>;
 };
 
 export default RobotaButton;
