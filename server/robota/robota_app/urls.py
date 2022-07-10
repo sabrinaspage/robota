f048987da9d68030260176b2c0df0028120f3e4b
YejuAhn <yeju12331@gmail.com>
2022-07-09 22:59:47 -0700

    Add companyjob api and disable duplicate in jobskill

--- server/robota/robota_app/urls.py
+++ server/robota/robota_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .views import (
-    CompanyApiView, JobSkillApiView
+    CompanyApiView, CompanyJobApiView, JobSkillApiView
 )
 
 urlpatterns = [
     path('company', CompanyApiView.as_view()),
+    path('company/job', CompanyJobApiView.as_view()),
     path('company/jobskill', JobSkillApiView.as_view())
 ]
--- server/robota/robota_app/views.py
+++ server/robota/robota_app/views.py
@@ -23,7 +23,7 @@ class CompanyApiView(APIView):
         Create a Company
         POST example
         {
-            "email": "google@google.com",
+            "email": "excompany@google.com",
             "description": "Tech",
             "name": "Google"
         }
@@ -43,6 +43,47 @@ class CompanyApiView(APIView):
             # invalid serializer
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({'error' : 'Company already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+class CompanyJobApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the Job skills
+        '''
+        companyJobs = CompanyJob.objects.all().values_list()
+        return Response(companyJobs, status=status.HTTP_200_OK)
+
+    # 2. Create
+    def post(self, request, *args, **kwargs):
+        '''
+        Create a Company Job
+        POST example
+        {
+            "email": "excompany@google.com",
+            "description" : "Responsible for maintaining the website.",
+            "name": "Software Engineer"
+        }
+        '''
+        # check if company exists
+        email = request.data.get('email')
+        company = Company.objects.filter(email = email)
+        if company.exists():
+            data = {
+                "company": company[0].id,
+                "description": request.data.get('description'),
+                "name": request.data.get('name')
+            }
+            serializer = CompanyJobSerializer(data=data)
+            # duplicate job allowed here
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+                # invalid serializer
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error' : 'Company does not exist'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class JobSkillApiView(APIView):
@@ -84,8 +125,11 @@ class JobSkillApiView(APIView):
                 }
                 serializer = JobSkillSerializer(data=data)
                 if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                    # check duplicate 
+                    if not JobSkill.objects.filter(companyJob = companyJob[0].id, name = request.data.get('name')).exists():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                    return Response({'error' : 'Job skill already exists'}, status=status.HTTP_400_BAD_REQUEST)
                 # invalid serializer
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             return Response({'error' : 'Company job does not exists'}, status=status.HTTP_400_BAD_REQUEST)
