b72535bac82c401ceebf61de3e2c6639cc55738d
YejuAhn <yeju12331@gmail.com>
2022-07-14 15:21:37 -0700

    Add id

--- server/robota/robota_app/serializers.py
+++ server/robota/robota_app/serializers.py
@@ -2,32 +2,42 @@ from rest_framework import serializers
 from .models import *
 
 class CompanySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = Company
-        fields = ["email", "description", "name", "password"]
+        fields = ["id" ,"email", "description", "name", "password"]
         
 class CompanyJobSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = CompanyJob
-        fields = ["company", "description", "name", "location"]
+        fields = ["id" ,"company", "description", "name", "location"]
 
 class JobSkillSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = JobSkill
-        fields = ["companyJob", "name"]
+        fields = ["id", "companyJob", "name"]
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["email", "fname", "lname", "password", "gender", "cv"]
+        fields = ["id" , "email", "fname", "lname", "password", "gender", "cv"]
         
 class JobUserSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = JobUser
-        fields = ["companyJob", "user", "status"]
+        fields = ["id" , "companyJob", "user", "status"]
 
 class UserSkillSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = UserSkill
-        fields = ["user", "name"]
+        fields = ["id", "user", "name"]
         
