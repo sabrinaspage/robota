26d42c7146ae4aadd3fa6ff4ac4cb946c927787f
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-07-16 01:24:22 -0400

    change type

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 interface FileInputProps {
   label: string;
   id: string;
-  onChange?: (event: any) => void;
+  onChange?: (event?: any) => void;
 }
 
 const FileInput = ({ label, id, onChange }: FileInputProps) => {
@@ -11,7 +11,13 @@ const FileInput = ({ label, id, onChange }: FileInputProps) => {
     <section className="w-100 p-4 d-flex justify-content-center pb-4">
       <div style={{ width: "22rem" }}>
         <label className="form-label">{label}</label>
-        <input type="file" className="form-control" id={id} autoComplete="" onChange = {onChange}/>
+        <input
+          type="file"
+          className="form-control"
+          id={id}
+          autoComplete=""
+          onChange={onChange}
+        />
       </div>
     </section>
   );
