09a5812d50bc2e2370c5b2ae9c3be0e6f3371202
Sabrina <54827482+sabrinaspage@users.noreply.github.com>
2022-07-16 23:38:03 -0400

    make sidebar clickable

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import GrayArrow from "../images/shapes/gray-arrow.png";
 import GrayPerson from "../images/shapes/gray-person.png";
 import GrayWindow from "../images/shapes/gray-window.png";
 import GrayClipboard from "../images/shapes/gray-clipboard.png";
+import { useLocation } from "react-router-dom";
 
 interface SidebarProps {
   currentUserType: string;
@@ -12,11 +13,12 @@ interface SidebarItemProps {
   title?: string;
   active?: boolean;
   imgUrl: string;
+  url?: string;
 }
 
-const SidebarItem = ({ title, active, imgUrl }: SidebarItemProps) => {
+const SidebarItem = ({ title, active, imgUrl, url }: SidebarItemProps) => {
   return (
-    <div
+    <a
       className="border rounded border-0"
       style={{
         backgroundColor: active ? "#FEFFBE" : "transparent",
@@ -25,16 +27,26 @@ const SidebarItem = ({ title, active, imgUrl }: SidebarItemProps) => {
         paddingTop: 15,
         paddingLeft: 10,
         cursor: "pointer",
+        textDecoration: "none",
+        color: "black",
       }}
+      href={url}
     >
       <img src={imgUrl} alt="" width="20" height="relative" />
       <span style={{ paddingLeft: 40 }}>{title}</span>
-    </div>
+    </a>
   );
 };
 
 const Sidebar = ({ currentUserType }: SidebarProps) => {
+  const currentPath = useLocation().pathname;
+
   if (currentUserType === "company") {
+    const companyList = [
+      { title: "Your Listings", url: "company-listings", imgUrl: GrayWindow },
+      { title: "Get Verified", url: "#", imgUrl: GrayArrow },
+      { title: "My Profile", url: "#", imgUrl: GrayPerson },
+    ];
     return (
       <div
         className="p-2 bd-highlight d-flex flex-column align-items-center"
@@ -43,29 +55,48 @@ const Sidebar = ({ currentUserType }: SidebarProps) => {
         <a href="/">
           <img src={RobotaLogo} alt="" width="100" height="relative" />
         </a>
-        <SidebarItem title="Your Listings" imgUrl={GrayWindow} active />
-        <p />
-        <SidebarItem title="Get Verified" imgUrl={GrayArrow} />
-        <p />
-        <SidebarItem title="My Profile" imgUrl={GrayPerson} />
+        {companyList.map((company) => (
+          <>
+            <SidebarItem
+              title={company.title}
+              imgUrl={company.imgUrl}
+              url={company.url}
+              active={currentPath === company.url}
+            />
+            <p />
+          </>
+        ))}
       </div>
     );
   }
+  const seekerList = [
+    { title: "Your Matches", url: "user-matches", imgUrl: GrayWindow },
+    { title: "Get Verified", url: "#", imgUrl: GrayArrow },
+    { title: "My Profile", url: "seeker-profile", imgUrl: GrayPerson },
+    { title: "Applications", url: "#", imgUrl: GrayClipboard },
+  ];
   return (
     <div
       className="p-2 bd-highlight d-flex flex-column align-items-center"
       style={{ backgroundColor: "white", width: 350 }}
     >
-      <a href="/">
-        <img src={RobotaLogo} alt="" width="100" height="relative" />
-      </a>
-      <SidebarItem title="Your Matches" imgUrl={GrayWindow} active />
-      <p />
-      <SidebarItem title="Get Verified" imgUrl={GrayArrow} />
-      <p />
-      <SidebarItem title="My Profile" imgUrl={GrayPerson} />
-      <p />
-      <SidebarItem title="Applications" imgUrl={GrayClipboard} />
+      <>
+        <a href="/">
+          <img src={RobotaLogo} alt="" width="100" height="relative" />
+        </a>
+        {console.log(seekerList.map((seeker) => seeker.url === currentPath))}
+        {seekerList.map((seeker) => (
+          <>
+            <SidebarItem
+              title={seeker.title}
+              imgUrl={seeker.imgUrl}
+              url={seeker.url}
+              active={currentPath === seeker.url}
+            />
+            <p />
+          </>
+        ))}
+      </>
     </div>
   );
 };
